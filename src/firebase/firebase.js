501e531a623bf47a3ca6f1c4db4e0458e4a84512
PiaSR <pia@richling.at>
2024-12-19 17:15:49 +0200

    updated .env keys

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCAIBGDgRq5CRFme1_aLDUEe-_RlgyaaZ8",
-  authDomain: "pump-pals.firebaseapp.com",
-  projectId: "pump-pals",
-  storageBucket: "pump-pals.appspot.com",
-  messagingSenderId: "276288538984",
-  appId: "1:276288538984:web:c88dc1e7fc72e634d0aedd",
-  measurementId: "G-B72K4M4J12"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
