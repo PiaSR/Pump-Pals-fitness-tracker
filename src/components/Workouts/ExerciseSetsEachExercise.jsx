47e1ee4bd6a11ffcf87380aca751cfb3df77116c
PiaSR <pia@richling.at>
2025-01-23 14:32:38 +0200

    updating adding sets

--- src/components/Workouts/ExerciseSetsEachExercise.jsx
+++ src/components/Workouts/ExerciseSetsEachExercise.jsx
@@ -5,25 +5,41 @@ import { useWorkout } from '../../contexts/workoutContexts/workoutContext'
 import { TiTick } from "react-icons/ti";
 import { useAuth } from '../../contexts/authContexts/authContext';
 import { db } from "/src/firebase/firebase.js" 
+import { PiPlusCircleThin } from "react-icons/pi";
+
+
+import { FaGripLines } from "react-icons/fa";
+
 
 import {  collection,  getDocs } from "firebase/firestore";
 
 
-const ExerciseSetsEachExercise = ({exercise, index}) => {
-	const [repsInput, setRepsInput] =useState(exercise.maxReps || "")
-	const [weightInput, setWeightInput] = useState(exercise.maxWeight || "")
+const ExerciseSetsEachExercise = ({exercise, index, key}) => {
+	
+	const [sets, setSets] = useState([
+		{ reps: exercise.maxReps || "", weight: exercise.maxWeight || "" },
+	  ]); 
 	
 
-	const handleRepsInput = (e) => {
-		setRepsInput(e.target.value)
-	}
-	const handleWeightInput = (e) => {
-		setWeightInput(e.target.value)
-	}
+	
+	const handleSetChange = (setIndex, key, value) => {
+		const updatedSets = sets.map((set, i) =>
+		  i === setIndex ? { ...set, [key]: value } : set
+		);
+		setSets(updatedSets);
+	  };
+
+	const handleAddAnotherSet = () => {
+		const lastSet = sets[sets.length - 1];
+		setSets([...sets, { reps: lastSet.reps, weight: lastSet.weight }]);
+	  };
+
+
+	
   return (
 	<div 
-			className='flex flex-col border border-solid border-gray-400 rounded-md p-5 my-6 bg-red-600 w-full '
-			key={exercise.id}
+			className='flex flex-col border border-solid border-gray-400 rounded-md p-5 my-6 w-full '
+			key={key}
 			>
 		<div className='flex justify-between'>
 			<div className='flex flex-col'>
@@ -34,16 +50,18 @@ const ExerciseSetsEachExercise = ({exercise, index}) => {
 			
 		</div>
 
-
-		<div className=' p-1 md:p-3 grid grid-cols-[auto_1fr_1fr_auto] justify-center items-center gap-3 md:gap-5 mt-3 w-full bg-yellow-400'>
-			<div className='w-8 h-8 flex justify-center items-center text-gray-400 rounded-full border-solid border border-gray-400 p-2'>{index+1}</div>
+		{sets.map((set, setIndex) => (
+		<div className=' p-1 md:p-3 grid grid-cols-[auto_1fr_1fr_auto] justify-center items-center gap-2  mt-3 w-full '>
+			<div className='w-7 h-7 flex justify-center items-center text-gray-400 rounded-full border-solid border border-gray-400 p-2 m-1 '>
+				{setIndex+1}
+			</div>
 
 			<input 
 			type="number"
-			value={repsInput}
-			onChange={handleRepsInput}
+			value={set.reps} //when new set is added, input fields should have same values as previous set
+			onChange={(e) => handleSetChange(setIndex, 'reps', e.target.value)}
 			placeholder={exercise.maxReps > 0 ? `Max: ${exercise.maxReps}` : "Reps"}
-			className='flex justify-center items-center border-gray-400 rounded-md border-solid border py-2 text-gray-500 bg-purple-500'>
+			className='flex justify-center items-center border-gray-400 rounded-md border-solid border p-2 text-gray-500  w-full'>
 				{/* {exercise.maxReps >0 
 				? {maxReps}
 				: "Reps"
@@ -52,10 +70,10 @@ const ExerciseSetsEachExercise = ({exercise, index}) => {
 			</input>
 			<input 
 			type="number"
-			value={weightInput}
-			onChange={handleWeightInput}
+			value={ set.weight} //when new set is added, input fields should have same values as previous set
+			onChange={(e) => handleSetChange(setIndex, 'weight', e.target.value)}
 			placeholder={exercise.maxWeight > 0 ? `Max: ${exercise.maxWeight}` : "Weight"}
-			className='flex justify-center items-center border-gray-400 rounded-md border-solid border py-2 text-gray-500'>
+			className='flex justify-center items-center border-gray-400 rounded-md border-solid border p-2 text-gray-500 w-full'>
 			{/* {exercise.maxWeight >0 
 				? {maxWeight}
 				: "Weight"
@@ -66,6 +84,17 @@ const ExerciseSetsEachExercise = ({exercise, index}) => {
 				<TiTick className='text-white text-2xl'/>
 			</div>
 
+		</div>))}
+
+		<div className='flex justify-between items-center relative mt-3'>
+		<PiPlusCircleThin 
+		className='text-gray-400 text-4xl cursor-pointer ml-1'
+		onClick={()=>handleAddAnotherSet()} />
+			<FaGripLines 
+			className='text-gray-400 mt-4 absolute justify-self-center left-1/2 -translate-x-1/2 cursor cursor-pointer'
+			 />
+				
+			
 		</div>
 	</div>
   )
